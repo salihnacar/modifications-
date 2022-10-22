52c8044db6b62889582e3bb2c3bc8761059a08bd
hawa <ragab5434@gmail.com>
2022-10-22 21:06:34 +0300

    re structure DB models >> Series, Season and epsoides and thier relationships

--- src/movie/models.py
+++ src/movie/models.py
@@ -131,18 +131,29 @@ class Series(models.Model):
     director = models.CharField(max_length=255)
     language = models.CharField(max_length=25, choices=series_language)
     actors = models.ManyToManyField(Actor, related_name='series_actors', blank=True)
-    seasons = models.ManyToManyField('Season', related_name='Series_Seasons', blank=True)
+    slug = models.SlugField(blank=True, null=True)
     
+    def save(self, *args, **kwargs):
+        # Logic
+        self.slug = slugify(self.name) 
+        super(Series,self).save(*args, **kwargs)
+
     def __str__(self):
         return self.name
 
 
 class Season(models.Model):
     name = models.CharField(max_length=100)
-    epsoide_count = models.ImageField()
+    epsoide_count = models.IntegerField()
     production_date = models.DateField()
-    series = models.ManyToManyField(Series, related_name='Series', blank=True)
-    epsoides = models.ForeignKey('Epsoide', related_name='Season_Epsoides', on_delete=models.CASCADE)
+    trailer = models.URLField(max_length=255)
+    series = models.ManyToManyField(Series, related_name='Series')
+    slug = models.SlugField(blank=True, null=True)
+    
+    def save(self, *args, **kwargs):
+        # Logic
+        self.slug = slugify(self.name) 
+        super(Season,self).save(*args, **kwargs)
     
     def __str__(self):
         return self.name
@@ -150,9 +161,17 @@ class Season(models.Model):
 
 class Epsoide(models.Model):
     name = models.CharField(max_length=150)
+    rate = models.DecimalField(max_digits=3, decimal_places=2)
+    season = models.ForeignKey(Season, related_name='season', on_delete=models.CASCADE)
     description = models.TextField(max_length=500)
-    trailer = models.URLField(max_length=255)
     subtitle = models.ForeignKey(Subtitle, related_name='epsoide_subtitle', on_delete=models.SET_NULL, null=True, blank=True)
+    slug = models.SlugField(blank=True, null=True)
+    
+    
+    def save(self, *args, **kwargs):
+        # Logic
+        self.slug = slugify(self.name) 
+        super(Epsoide,self).save(*args, **kwargs)
     
     def __str__(self):
         return self.name
