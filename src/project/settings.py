839960420f2860ea85bfa5cb3d002c22c9387069
hawa <ragab5434@gmail.com>
2022-10-31 14:54:14 +0300

    updated settings for deployment

--- src/project/settings.py
+++ src/project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-td_bq@3yt*&mi#miw1i7j+7cgb$cas346*$@jhpc48ig7_!lm0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -131,6 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
