b26c5b42fd1b2a5ece6d59d02ff01b0b746e923b
hawa <ragab5434@gmail.com>
2022-10-31 14:32:05 +0300

    nothing

--- src/movie/admin.py
+++ src/movie/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import MovieType, Movie, Subtitle, Actor, Series, Season, Epsoide
 
+
 # Register your models here.
 admin.site.register(MovieType)
 admin.site.register(Movie)
